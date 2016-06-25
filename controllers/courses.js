9f481fef87881b6617a2c50a1c2f03d1c5446630
L-u-k-e <parzycl1@sunyit.edu>
2016-06-25 16:39:57 -0400

    remove a log statement on the courses controller

--- controllers/courses.js
+++ controllers/courses.js
@@ -21,7 +21,6 @@ model.exports = function (router) {
 };*/
 
 var get_some = function (req, res, next) {
-	console.log(req.query);
 	courses.get_some(req.query)
 	.then(function (data) {
 		res.status(200).json({
