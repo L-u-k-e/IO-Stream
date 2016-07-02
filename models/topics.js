12f946cbfc61864063ce0c542b05dc98ac78b45b
L-u-k-e <parzycl1@sunyit.edu>
2016-07-02 17:38:59 -0400

    implement crude subject lookup in the topic model

--- models/topics.js
+++ models/topics.js
@@ -1,10 +1,13 @@
-var db = require('../config/db_config');
-var orm = require('../helpers/orm');
+var db            = require('../config/db_config');
+var orm           = require('../helpers/orm');
 var generate_uuid = require('../helpers/uuid');
+var _             = require('lodash');
+var async         = require('async');
+
 var table = 'topic';
 
 exports.get_some = function (args) {
-	var promise = orm.select({
+	var topics_promise = orm.select({
 		db:     db, 
 		table:  table,  
 		where:  args.where,
@@ -13,15 +16,40 @@ exports.get_some = function (args) {
 		offset: args.offset,
 		group:  args.group
 	});
-	return promise;
+
+	var topics_with_subjects_promise = topics_promise.map(function (topic) {
+		var promise = orm.select({
+			db: db,
+			table: 'subject',
+			qrm: 'one',
+			where: {id: topic.subject_id}
+		}).then(function (subject) {
+			topic.subject = subject.title;
+			return topic;
+		});
+		return promise;
+	});
+
+	return topics_with_subjects_promise;
 };
 
 exports.get_one = function (args) {
-	var promise = orm.select({
+	var topic_promise = orm.select({
 		db: db,
 		table: table,
-		where: {items: {id: args.id}, delimiter: 'AND'},
+		where: {id: args.id},
 		qrm: 'one'
+	}).then(function(topic) {
+		var promise = orm.select({
+			db: db,
+			table: 'subject',
+			where: { id: topic.subject_id },
+			qrm: 'one'
+		}).then(function (subject) {
+			topic.subject = subject.title;
+			return topic;
+		});
+		return promise;
 	});
-	return promise;
+	return topic_promise;
 };
