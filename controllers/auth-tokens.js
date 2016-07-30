357b5ade9e142bf6a28eb1d52b27a6bf8b492b34
L-u-k-e <parzycl1@sunyit.edu>
2016-07-30 16:43:37 -0400

    add a success callback queue to the signin dialog

--- controllers/auth-tokens.js
+++ controllers/auth-tokens.js
@@ -6,12 +6,12 @@ var authenticate = function (req, res, next) {
 	people.authenticate(creds)
 	.then(function (result) {
 		if (!result.valid) { 
-			res.status(401).json({message: "invalid username or password"});
+			res.status(401).json({message: "Invalid username or password."});
 		} else {
 			var fresh_token = token.new(result.user);
 			var cookie_opts = { maxAge: (1000 * 60 * 60 * 24 * 7), httpOnly: true };
 			res.cookie('auth_token', fresh_token, cookie_opts);
-			res.status(201).json({message: "created a new authentication token"});
+			res.status(201).json({message: "Created a new authentication token."});
 		}
 	});
 };
