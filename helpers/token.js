2afc086ee98141b0bddbecbae8ada31a9026a94f
Luke <hi.lucas.p@gmail.com>
2016-08-14 16:31:30 -0400

    modularize helpers/token

--- helpers/token.js
+++ helpers/token.js
@@ -1,32 +1,104 @@
-var jwt = require('jwt-simple');
-var _   = require('lodash');
+var jwt    = require('jwt-simple');
+var moment = require('moment');
+var _      = require('lodash');
+var people = require('../models/people');
 
 var secret = 'donuts';
 
-exports.new = function (user) {
-	var payload = {user: user};
+
+
+
+
+/******************************** MIDDLEWARE *********************************/
+
+var make_token = function(user) {
+	var payload = {
+		iat: moment().format('X'),
+		user: user};
 	var token = jwt.encode(payload, secret);
 	return token;
 };
 
-exports.auth = function (perms) {
-	var middleware = function (req, res, next) {
-		try {
-			var token = req.headers.authorization;
-			var payload = jwt.decode(token, secret);
 
-			var has_perm = function (perm) {
-				var prop = _.snakeCase(perm);
-				var has = !!payload.user[prop];
-				return has;
-			};
+function auth(perms) {
+	return [
+		decode_token,
+		check_token_expiry,
+		check_route_permissions(perms),
+		refresh_token_if_necessary,
+	];
+}
 
-			if (_.every(perms, has_perm)) next();
-			else res.status(401).json({message: 'Insufficient permissions.'});
-			
-		} catch (err) {
-			res.status(401).json({message: 'Invalid or non-existant authentication token.'});
-		}
+
+function decode_token(req, res, next) {
+	try {
+		var token = {encoded: req.headers.authorization};
+		token.decoded = jwt.decode(token.encoded, secret);
+		res.locals.token = token;
+		next();
+	} catch (err) {
+		send_invalid(res, 401);
+	}
+}
+
+
+function check_token_expiry(req, res, next) {
+	people.get({
+		inflection: 'one',
+		where: {id: res.locals.token.decoded.user.id}
+	}).then(function (person) {
+		console.log(moment(person.last_token_invalidation).format('X'));
+		next();
+	}).catch(function (err) {
+		send_invalid(res, 401);
+	});	
+}
+
+
+function check_route_permissions(perms) {
+	perms = perms || [];
+	return function (req, res, next) {
+		var user_has_perm = _.partial(has_perm, res.locals.token.decoded.user);
+		if (_.every(perms, user_has_perm)) next();
+		else send_invalid(res, 403);
 	};
-	return middleware;
+}
+
+
+function refresh_token_if_necessary(req, res, next) {
+	next();
+}
+
+
+
+
+
+/********************************** HELPERS **********************************/
+
+function has_perm(user, perm) {
+	var prop = _.snakeCase(perm);
+	var has = !!user[perm];
+	return has;
+}
+
+
+function send_invalid(res, status) {
+	var message = '';
+	if (status == 401) message = 'Invalid or non-existant authentication token.';
+	else if (status == 403) message = 'Insufficient permissions.';
+	res.status(status).json({message: message});
+}
+
+
+
+
+
+/********************************** EXPORTS **********************************/
+module.exports = {
+	new: make_token,
+	auth: auth,
+	decode_token: decode_token,
+	check_token_expiry: check_token_expiry,
+	check_route_permissions: check_route_permissions,
+	refresh_token_if_necessary: refresh_token_if_necessary
 };
