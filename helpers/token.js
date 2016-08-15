be233cc931a0d9739969b39b338dcd2f4327b3f2
Luke <hi.lucas.p@gmail.com>
2016-08-15 05:47:04 -0400

    implement token refresh and date check

--- helpers/token.js
+++ helpers/token.js
@@ -12,22 +12,28 @@ var secret = 'donuts';
 /******************************** MIDDLEWARE *********************************/
 
 var make_token = function(user) {
+	var now = moment();
 	var payload = {
-		iat: moment().format('X'),
-		user: user};
+		iat: now.format('X'),
+		exp: now.add(7, 'days').format('X'),
+		user: user
+	};
 	var token = jwt.encode(payload, secret);
 	return token;
 };
 
 
+
 function auth(perms) {
 	return [
 		decode_token,
-		check_token_expiry,
+		get_token_person,
+		check_token_dates,
 		check_route_permissions(perms),
 		refresh_token_if_necessary,
 	];
 }
+
 
 
 function decode_token(req, res, next) {
@@ -37,22 +43,34 @@ function decode_token(req, res, next) {
 		res.locals.token = token;
 		next();
 	} catch (err) {
+		// signature doesn't match
 		send_invalid(res, 401);
 	}
 }
 
 
-function check_token_expiry(req, res, next) {
+
+function get_token_person(req, res, next) {
 	people.get({
 		inflection: 'one',
 		where: {id: res.locals.token.decoded.user.id}
 	}).then(function (person) {
-		console.log(moment(person.last_token_invalidation).format('X'));
+		res.locals.person = person;
 		next();
 	}).catch(function (err) {
+		// user was deleted after the token was issued
 		send_invalid(res, 401);
 	});	
 }
+
+
+
+function check_token_dates(req, res, next) {
+	var dates = get_key_dates(res);
+	if (dates.issue_date.isAfter(dates.valid_after) && dates.issue_date.isBefore(dates.exp_date)) next();
+	else send_invalid(res, 401);
+}
+
 
 
 function check_route_permissions(perms) {
@@ -65,7 +83,12 @@ function check_route_permissions(perms) {
 }
 
 
+
 function refresh_token_if_necessary(req, res, next) {
+	console.log('starting');
+	var dates = get_key_dates(res);
+	var lapsed = dates.issue_date.diff(moment(), 'days');
+	if (lapsed >= 1) res.headers.authentication = make_token(res.person);
 	next();
 }
 
@@ -75,11 +98,23 @@ function refresh_token_if_necessary(req, res, next) {
 
 /********************************** HELPERS **********************************/
 
+function get_key_dates(res) {
+	var dates = {
+		valid_after: moment(res.locals.person.last_token_invalidation),
+		issue_date: moment.unix(res.locals.token.decoded.iat),
+		exp_date: moment.unix(res.locals.token.decoded.exp)
+	};
+	return dates;
+}
+
+
+
 function has_perm(user, perm) {
 	var prop = _.snakeCase(perm);
 	var has = !!user[perm];
 	return has;
 }
+
 
 
 function send_invalid(res, status) {
@@ -98,7 +133,8 @@ module.exports = {
 	new: make_token,
 	auth: auth,
 	decode_token: decode_token,
-	check_token_expiry: check_token_expiry,
+	get_token_person: get_token_person,
+	check_token_dates: check_token_dates,
 	check_route_permissions: check_route_permissions,
 	refresh_token_if_necessary: refresh_token_if_necessary
 };
